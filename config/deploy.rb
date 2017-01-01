737247fdc6371446d5efce3d4268dcf2ac17fed1
Małgorzata Ksionek <meksionek@gmail.com>
2017-01-01 19:14:14 +0100

    One step back

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,9 +36,7 @@ set :rack_env, :production
 # Unicorn control tasks
 namespace :deploy do
   task :restart do
-    on "#{fetch(:host)}" do
-      execute "if [ -f #{fetch(:unicorn_pid)} ]; then kill -USR2 `cat #{fetch(:unicorn_pid)}`; else cd #{fetch(:current_path)} && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rack_env)} -D; fi"
-    end
+    invoke 'unicorn:restart'
   end
   # task :start do
   #   run "cd #{fetch(:deploy_to)}/current && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rake_env)} -D"
--- unicorn.rb
+++ unicorn.rb
@@ -1,46 +1,33 @@
 # define paths and filenames
-deploy_to = "/home/app/adder_web"
-rails_root = "#{deploy_to}/current"
-pid_file = "#{deploy_to}/shared/pids/unicorn.pid"
-socket_file= "#{deploy_to}/shared/unicorn.sock"
-log_file = "#{rails_root}/log/unicorn.log"
-err_log = "#{rails_root}/log/unicorn_error.log"
-old_pid = pid_file + '.oldbin'
-working_directory rails_root
+app_path = "/home/app/adder_web"
 
-# worker_processes 3
-# working_directory current_path
-# listen "/var/tmp/unicorn.sock", backlog: 64
-# timeout 180
+# paths
+working_directory "#{app_path}/current"
+pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
-# pid "#{shared_path}/tmp/pids/unicorn.pid"
+# listen
+listen "#{app_path}/current/tmp/sockets/unicorn.sock", :backlog => 64
 
-# stderr_path "#{shared_path}/log/unicorn.stderr.log"
-# stdout_path "#{shared_path}/log/unicorn.stdout.log"
+# logging
+stderr_path "#{app_path}/current/log/unicorn.stderr.log"
+stdout_path "#{app_path}/current/log/unicorn.stdout.log"
 
-before_exec do |_server|
-  ENV['BUNDLE_GEMFILE'] = "#{current_path}/Gemfile"
+# workers
+worker_processes 3
+
+# use correct Gemfile on restarts
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
 end
 
-timeout 30
-worker_processes 2 # increase or decrease
-listen socket_file, :backlog => 1024
-
-pid pid_file
-stderr_path err_log
-stdout_path log_file
-
-# make forks faster
-
-# make sure that Bundler finds the Gemfile
-# before_exec do |server|
-#   ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
-# end
+# preload
+preload_app true
 
 before_fork do |server, worker|
 
-  # zero downtime deploy magic:
-  # if unicorn is already running, ask it to start a new process and quit.
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{server.config[:pid]}.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
